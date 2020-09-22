c5e582b745b8c3303df69ca1d2916cdcb561a07b
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-09-22 09:40:01 +0700

    Add files via upload

---
+++ tugas32.js
@@ -0,0 +1,10 @@
+function nama(){
+var depan = document.getElementById('namapertama')
+var belakang = document.getElementById('namabelakang')
+var hobi = document.getElementById('hobi')
+
+console.log(depan)
+console.log(belakang)
+console.log(hobi)
+
+}
